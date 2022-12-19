b7aa0cce2934336beb3820cd29ca2e450e2c638d
Sergei Romanov <90252340+zoneout215@users.noreply.github.com>
2022-12-19 17:51:48 +0100

    Add files via upload

--- network.py
+++ network.py
@@ -1,47 +1,43 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-
 data = pd.read_csv('train.csv')
 
 data = np.array(data)
-m, n = data.shape
-np.random.shuffle(data) # shuffle before splitting into dev and training sets
+m,n = data.shape
+np.random.shuffle(data)
 
 data_dev = data[0:1000].T
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
-X_dev = X_dev / 255.
+X_dev = X_dev / 255
 
 data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
-X_train = X_train / 255.
+X_train = X_train / 255
 _,m_train = X_train.shape
 
 def init_params():
-    W1 = np.random.rand(10, 784) - 0.5
-    b1 = np.random.rand(10, 1) - 0.5
-    W2 = np.random.rand(10, 10) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
+    W1 = np.random.randn(10, 784) -0.5
+    b1 =  np.random.randn(10, 1) -0.5
+    W2 = np.random.randn(10, 10) -0.5
+    b2 =  np.random.randn(10, 1) -0.5
     return W1, b1, W2, b2
 
-def ReLU(Z):
-    return np.maximum(Z, 0)
+def ReLV(Z):
+    return np.maximum(Z,0)
 
 def softmax(Z):
-    A = np.exp(Z) / sum(np.exp(Z))
+    A = np.exp(Z)/sum(np.exp(Z))
     return A
-    
-def forward_prop(W1, b1, W2, b2, X):
-    Z1 = W1.dot(X) + b1
-    A1 = ReLU(Z1)
-    Z2 = W2.dot(A1) + b2
-    A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
 
-def ReLU_deriv(Z):
-    return Z > 0
+def forward_propagation(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLV(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(np.array([1, 1]))
+    return Z1, A1, Z2, A2 
 
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
@@ -49,40 +45,45 @@ def one_hot(Y):
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+def derivative_ReLV(Z):
+    return Z > 0
+
+def backward_propagation(Z1, A1, Z2, A2, W1, W2, Y, X):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
-    dW2 = 1 / m * dZ2.dot(A1.T)
-    db2 = 1 / m * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
-    dW1 = 1 / m * dZ1.dot(X.T)
-    db1 = 1 / m * np.sum(dZ1)
-    return dW1, db1, dW2, db2
+    dW2 = 1/ m * dZ2.dot(A1.T)
+    db2 = 1/ m * np.sum(dZ2) 
+    dZ1 = W2.T.dot(dZ2) * derivative_ReLV(Z1)
+    dW1 = 1/m * dZ1.dot(X.T)
+    db1 = 1/m * np.sum(dZ1)
+    return dW1, dW2, db1, db2
 
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+def update_params(W1, b1, W2,  b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
-    b1 = b1 - alpha * db1    
-    W2 = W2 - alpha * dW2  
-    b2 = b2 - alpha * db2    
-    return W1, b1, W2, b2
+    b1 = W1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, W2, b1, b2
 
 def get_predictions(A2):
     return np.argmax(A2, 0)
 
 def get_accuracy(predictions, Y):
     print(predictions, Y)
-    return np.sum(predictions == Y) / Y.size
+    return np.sum(predictions ==Y)/Y.size
 
-def gradient_descent(X, Y, alpha, iterations):
+def gradient_descending(X, Y, iterations, alpha):
+    """ 
+    Implements gradient descending
+    """
     W1, b1, W2, b2 = init_params()
     for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-        if i % 10 == 0:
-            print("Iteration: ", i)
-            predictions = get_predictions(A2)
-            print(get_accuracy(predictions, Y))
+        Z1, A1, Z2, A2 =  forward_propagation(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_propagation(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2,  b2 = update_params(W1,b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i %  50 == 0:
+            print('iteration: ', i )
+            print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
     return W1, b1, W2, b2
 
-W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+W1, b1, W2, b2 = gradient_descending(X_train, Y_train, 500, 0.1)
