54f016cc764181a53bd468359b28d3c73d13d02e
Sergei Romanov <90252340+zoneout215@users.noreply.github.com>
2022-12-19 18:28:55 +0100

    error fixed
    spotted the error

--- network.py
+++ network.py
@@ -2,23 +2,6 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-data = pd.read_csv('train.csv')
-
-data = np.array(data)
-m, n = data.shape
-np.random.shuffle(data)
-
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:n]
-X_dev = X_dev / 255.
-
-data_train = data[1000:m].T
-Y_train = data_train[0]
-X_train = data_train[1:n]
-X_train = X_train / 255.
-_,m_train = X_train.shape
-
 def init_params():
     W1 = np.random.rand(10, 784) - 0.5
     b1 = np.random.rand(10, 1) - 0.5
@@ -88,5 +71,24 @@ def gradient_descend(X, Y, alpha, iterations):
             predictions = get_predictions(A2)
             print("Accuracy: ", get_accuracy(predictions, Y))
     return W1, b1, W2, b2
+
+data = pd.read_csv('train.csv')
+
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
+
+
 
 W1, b1, W2, b2 = gradient_descend(X_train, Y_train, 0.1, 500)
